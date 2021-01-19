a8fb97a9d093f106ae851d261a4f9076e3a5f810
denizaral <araldeniz97@gmail.com>
2021-01-19 18:29:00 +0300

    Project

---
+++ If-Elif-Else.py
@@ -0,0 +1,13 @@
+// If-Elif-Else Simple Example
+
+number = int(input("Enter number."))
+
+if number>0:
+    print("Pozitive")
+
+elif number == 0:
+    print("Zero")
+    
+else:
+    print("Negative")
+
